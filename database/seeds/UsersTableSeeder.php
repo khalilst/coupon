5fd4caea0008f6ade060a8a1fbb6eaceccf6a8f2
Xalil Laleh <khalilst@gmail.com>
2020-03-14 22:13:03 +0330

    Make UsersTableSeeder to generate one admin and one normal user

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //Create an admin
+        factory(User::class)->create(['email' => 'admin@coupon.test']);
+
+        //Create a normal user
+        factory(User::class)->create(['email' => 'user@coupon.test']);
+    }
+}
